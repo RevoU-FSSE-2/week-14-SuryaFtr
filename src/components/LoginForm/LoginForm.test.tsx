bc2f0fd7be4db81a97688f53d746de1db6e24ae9
Surya Faturohman <suryafaturohman@gmail.com>
2023-09-30 03:06:17 +0700

    Fix Unit Testing Login Form

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { waitFor, fireEvent, render, screen } from '@testing-library/react'
 import LoginForm from '.';
 
 describe('test login form', () => {
@@ -20,9 +20,29 @@ describe('test login form', () => {
         expect(form).toBeDefined();
     })
 
-    test('Submit button render correctly', async () => {
+    test('Login button render correctly', async () => {
         render(<LoginForm onSubmit={mockProps} />)
         const title = screen.getByText('Login')
         expect(title).toBeDefined();
     })
+
+    test('onSubmit works correctly', async () => {
+        const { getByPlaceholderText, getByText } = render(<LoginForm onSubmit={mockProps} />);
+        const emailInput = getByPlaceholderText('Enter Your Email') as HTMLInputElement;
+        const passwordInput = getByPlaceholderText('Enter Your Password') as HTMLInputElement;
+        const submitButton = getByText('Login');
+
+        fireEvent.change(emailInput, { target: { value: 'testemail123@email.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'testpassword123' } });
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(mockProps).toHaveBeenCalledTimes(1);
+            expect(mockProps).toHaveBeenCalledWith({
+                email: 'testemail123@email.com',
+                password: 'testpassword123',
+            });
+        });
+    });
 })
