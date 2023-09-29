090a47cc7122428f2668d5ad9f3a25ab5d36213a
Surya Faturohman <suryafaturohman@gmail.com>
2023-09-30 02:41:25 +0700

    Add Unit Testing CategoryList

---
+++ src/components/CategoryList/CategoryList.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from '@testing-library/react';
+import { ColumnsType } from 'antd/es/table';
+import { Category } from '../../types';
+import CategoryList from '.';
+
+const columns: ColumnsType<Category> = [
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Status',
+        dataIndex: 'is_active',
+        key: 'is_active',
+    }
+];
+
+describe('Test Category List Component', () => {
+    beforeAll(() => {
+        Object.defineProperty(window, 'matchMedia', {
+            writable: true,
+            value: jest.fn().mockImplementation(query => ({
+                matches: false,
+                media: query,
+                onchange: null,
+                addListener: jest.fn(), // Deprecated
+                removeListener: jest.fn(), // Deprecated
+                addEventListener: jest.fn(),
+                removeEventListener: jest.fn(),
+                dispatchEvent: jest.fn(),
+            })),
+        });
+    })
+    test('test header column', () => {
+        render(<CategoryList columns={columns} data={[]} />)
+
+        columns.map((column) => {
+            if (column.title) {
+                const title = screen.getByText(column.title.toString());
+                expect(title).toBeDefined()
+            }
+        })
+    })
+})
