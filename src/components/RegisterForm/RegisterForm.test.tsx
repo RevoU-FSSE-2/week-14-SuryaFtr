8d4bcd8bec165a9ba38d522755854d2b33a1d18a
Surya Faturohman <suryafaturohman@gmail.com>
2023-09-30 03:10:41 +0700

    Fix Unit Testing RegisterForm

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { waitFor, fireEvent, render, screen } from '@testing-library/react'
 import RegisterForm from '.';
 
 describe('test register form', () => {
@@ -33,4 +33,27 @@ describe('test register form', () => {
         const title = screen.getByText('Submit')
         expect(title).toBeDefined();
     })
+
+    test('onSubmit works correctly', async () => {
+        const { getByPlaceholderText, getByText } = render(<RegisterForm onSubmit={mockProps} />);
+        const nameInput = getByPlaceholderText('Enter Your Name') as HTMLInputElement;
+        const emailInput = getByPlaceholderText('Enter Your Email') as HTMLInputElement;
+        const passwordInput = getByPlaceholderText('Enter Your Password') as HTMLInputElement;
+        const submitButton = getByText('Submit');
+
+        fireEvent.change(nameInput, { target: { value: 'testname' } });
+        fireEvent.change(emailInput, { target: { value: 'testemail123@email.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'testpassword123' } });
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(mockProps).toHaveBeenCalledTimes(1);
+            expect(mockProps).toHaveBeenCalledWith({
+                name: 'testname',
+                email: 'testemail123@email.com',
+                password: 'testpassword123',
+            });
+        });
+    });
 })
